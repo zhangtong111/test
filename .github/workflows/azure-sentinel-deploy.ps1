4bb817a6f038cf4dbd8fb5adf39418373071d921
debjyoti11 <82176448+debjyoti11@users.noreply.github.com>
2021-06-30 15:20:32 -0700

    Sentinel Content Deployment Script

--- .github/workflows/azure-sentinel-deploy.ps1
+++ .github/workflows/azure-sentinel-deploy.ps1
@@ -18,13 +18,61 @@ function ConnectAzCloud {
     Set-AzContext -Tenant $RawCreds.tenantId | out-null;
 }
 
+function IsValidTemplate($path) {
+    Try {
+        Test-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $path -workspace $Env:workspaceName
+        return $true
+    }
+    Catch {
+        Write-Host "[Warning] The file $path is not valid: $_"
+        return $false
+    }
+}
+
 if ($Env:cloudEnv -ne 'AzureCloud') {
     Write-Output "Attempting Sign In to Azure Cloud"
     ConnectAzCloud
 }
 
 Write-Output "Starting Deployment for Files in path: $Env:directory"
-Get-ChildItem $Env:directory -Filter *.json |
-ForEach-Object {
-    New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $_.FullName -logAnalyticsWorkspaceName $Env:workspaceName
+$MaxRetries = 3;
+
+if (Test-Path -Path $Env:directory) {
+    $totalFiles = 0;
+    $totalFailed = 0;
+    Get-ChildItem -Path $Env:directory -Recurse -Filter *.json |
+    ForEach-Object {
+        $CurrentFile = $_.FullName
+        $totalFiles ++
+        $isValid = IsValidTemplate $CurrentFile
+        if (-not $isValid) {
+            $totalFailed++
+            return
+        }
+        $isSuccess = $false
+        $currentAttempt = 1
+        While (($currentAttempt -le $MaxRetries) -and (-not $isSuccess)) {
+            Write-Output "Deploying $CurrentFile, attempt $currentAttempt of $MaxRetries"
+            $currentAttempt ++
+            Try {
+                New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $CurrentFile -workspace $Env:workspaceName
+                $isSuccess = $true
+            }
+            Catch {        
+                Write-Output "[Warning] Failed to deploy $CurrentFile with error: $_"
+                $isSuccess = $false
+            }
+        }
+        if (-not $isSuccess) {
+            $totalFailed++
+            Write-Output "[Warning] Unable to deploy $CurrentFile. Deployment failed after $MaxRetries unsuccessful attempts."
+        }
+    }
+    if ($totalFiles -gt 0 -and $totalFailed -gt 0) {
+        $error = "$totalFailed of $totalFiles deployments failed."
+        Throw $error
+    }
+}
+else {
+    Write-Output "[Warning] $Env:directory not found. nothing to deploy"
 }
