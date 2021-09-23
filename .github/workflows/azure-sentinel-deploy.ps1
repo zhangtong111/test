3a49436733985f17a811113ef9d6ce5a7e9b0a5a
zhangtong111 <89553124+zhangtong111@users.noreply.github.com>
2021-09-22 18:58:56 -0700

    Sentinel Content Deployment Script

--- .github/workflows/azure-sentinel-deploy.ps1
+++ .github/workflows/azure-sentinel-deploy.ps1
@@ -1,11 +1,47 @@
+## Globals ##
+$CloudEnv = $Env:cloudEnv
+$ResourceGroupName = $Env:resourceGroupName
+$WorkspaceName = $Env:workspaceName
+$Directory = $Env:directory
+$Creds = $Env:creds
+
+$MaxRetries = 3
+$secondsBetweenAttempts = 5
+
+function AttemptAzLogin($psCredential, $tenantId, $cloudEnv) {
+    $maxLoginRetries = 3
+    $delayInSeconds = 30
+    $retryCount = 1
+    $stopTrying = $false
+    do {
+        try {
+            Connect-AzAccount -ServicePrincipal -Tenant $tenantId -Credential $psCredential -Environment $cloudEnv | out-null;
+            Write-Host "Login Successful"
+            $stopTrying = $true
+        }
+        catch {
+            if ($retryCount -ge $maxLoginRetries) {
+                Write-Host "Login failed after $maxLoginRetries attempts."
+                $stopTrying = $true
+            }
+            else {
+                Write-Host "Login attempt failed, retrying in $delayInSeconds seconds."
+                Start-Sleep -Seconds $delayInSeconds
+                $retryCount++
+            }
+        }
+    }
+    while (-not $stopTrying)
+}
+
 function ConnectAzCloud {
-    $RawCreds = $Env:creds | ConvertFrom-Json
+    $RawCreds = $Creds | ConvertFrom-Json
 
     Clear-AzContext -Scope Process;
     Clear-AzContext -Scope CurrentUser -Force -ErrorAction SilentlyContinue;
     
     Add-AzEnvironment `
-        -Name $Env:cloudEnv `
+        -Name $CloudEnv `
         -ActiveDirectoryEndpoint $RawCreds.activeDirectoryEndpointUrl `
         -ResourceManagerEndpoint $RawCreds.resourceManagerEndpointUrl `
         -ActiveDirectoryServiceEndpointResourceId $RawCreds.activeDirectoryServiceEndpointResourceId `
@@ -14,13 +50,13 @@ function ConnectAzCloud {
     $servicePrincipalKey = ConvertTo-SecureString $RawCreds.clientSecret.replace("'", "''") -AsPlainText -Force
     $psCredential = New-Object System.Management.Automation.PSCredential($RawCreds.clientId, $servicePrincipalKey)
 
-    Connect-AzAccount -ServicePrincipal -Tenant $RawCreds.tenantId -Credential $psCredential -Environment $Env:cloudEnv | out-null;
+    AttemptAzLogin $psCredential $RawCreds.tenantId $CloudEnv
     Set-AzContext -Tenant $RawCreds.tenantId | out-null;
 }
 
 function IsValidTemplate($path) {
     Try {
-        Test-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $path -workspace $Env:workspaceName
+        Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $path -workspace $WorkspaceName
         return $true
     }
     Catch {
@@ -29,50 +65,90 @@ function IsValidTemplate($path) {
     }
 }
 
-if ($Env:cloudEnv -ne 'AzureCloud') {
-    Write-Output "Attempting Sign In to Azure Cloud"
-    ConnectAzCloud
-}
-
-Write-Output "Starting Deployment for Files in path: $Env:directory"
-$MaxRetries = 3;
-
-if (Test-Path -Path $Env:directory) {
-    $totalFiles = 0;
-    $totalFailed = 0;
-    Get-ChildItem -Path $Env:directory -Recurse -Filter *.json |
-    ForEach-Object {
-        $CurrentFile = $_.FullName
-        $totalFiles ++
-        $isValid = IsValidTemplate $CurrentFile
-        if (-not $isValid) {
-            $totalFailed++
-            return
-        }
-        $isSuccess = $false
-        $currentAttempt = 1
-        While (($currentAttempt -le $MaxRetries) -and (-not $isSuccess)) {
-            Write-Output "Deploying $CurrentFile, attempt $currentAttempt of $MaxRetries"
-            $currentAttempt ++
-            Try {
-                New-AzResourceGroupDeployment -ResourceGroupName $Env:resourceGroupName -TemplateFile $CurrentFile -workspace $Env:workspaceName
-                $isSuccess = $true
-            }
-            Catch {        
-                Write-Output "[Warning] Failed to deploy $CurrentFile with error: $_"
-                $isSuccess = $false
-            }
-        }
-        if (-not $isSuccess) {
-            $totalFailed++
-            Write-Output "[Warning] Unable to deploy $CurrentFile. Deployment failed after $MaxRetries unsuccessful attempts."
-        }
+function IsRetryable($deploymentName) {
+    $retryableStatusCodes = "Conflict","TooManyRequests","InternalServerError"
+    Try {
+        $deploymentResult = Get-AzResourceGroupDeploymentOperation -DeploymentName $deploymentName -ResourceGroupName $ResourceGroupName -ErrorAction Stop
+        return $retryableStatusCodes -contains $deploymentResult.StatusCode
     }
-    if ($totalFiles -gt 0 -and $totalFailed -gt 0) {
-        $error = "$totalFailed of $totalFiles deployments failed."
-        Throw $error
+    Catch {
+        return $false
     }
 }
-else {
-    Write-Output "[Warning] $Env:directory not found. nothing to deploy"
+
+function AttemptDeployment($path, $deploymentName) {
+    $isValid = IsValidTemplate $path
+    if (-not $isValid) {
+        return $false
+    }
+    $isSuccess = $false
+    $currentAttempt = 0
+    While (($currentAttempt -lt $MaxRetries) -and (-not $isSuccess)) 
+    {
+        $currentAttempt ++
+        Try 
+        {
+
+            New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $path -workspace $workspaceName -ErrorAction Stop | Out-Host
+            $isSuccess = $true
+        }
+        Catch [Exception] 
+        {
+            $error = $_
+            if (-not (IsRetryable $deploymentName)) 
+            {
+                Write-Host "[Warning] Failed to deploy $path with error: $error"
+                break
+            }
+            else 
+            {
+                if ($currentAttempt -le $MaxRetries) 
+                {
+                    Write-Host "[Warning] Failed to deploy $path with error: $error. Retrying in $secondsBetweenAttempts seconds..."
+                    Start-Sleep -Seconds $secondsBetweenAttempts
+                }
+                else
+                {
+                    Write-Host "[Warning] Failed to deploy $path after $currentAttempt attempts with error: $error"
+                }
+            }
+        }
+    }
+    return $isSuccess
 }
+
+function main() {
+    if ($CloudEnv -ne 'AzureCloud') 
+    {
+        Write-Output "Attempting Sign In to Azure Cloud"
+        ConnectAzCloud
+    }
+
+    Write-Output "Starting Deployment for Files in path: $Directory"
+
+    if (Test-Path -Path $Directory) 
+    {
+        $totalFiles = 0;
+        $totalFailed = 0;
+        Get-ChildItem -Path $Directory -Recurse -Filter *.json |
+        ForEach-Object {
+            $totalFiles ++
+            $isSuccess = AttemptDeployment $_.FullName $_.Basename 
+            if (-not $isSuccess) 
+            {
+                $totalFailed++
+            }
+        }
+        if ($totalFiles -gt 0 -and $totalFailed -gt 0) 
+        {
+            $error = "$totalFailed of $totalFiles deployments failed."
+            Throw $error
+        }
+    }
+    else 
+    {
+        Write-Output "[Warning] $Directory not found. nothing to deploy"
+    }
+}
+
+main
